a689508ba835456ee7285d6c0c7ee5f160cf10fa
Erlan <erlanchuacbaev@gmail.com>
2023-07-05 14:49:16 +0600

    bdd+specflow

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,11 +4,11 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double eps = 1e-9;
+        double eps = 1e-6;
 
         if ((a > -eps && a < eps) || double.IsNaN(a) || double.IsNaN(b) || double.IsNaN(c) || double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(c))
         {
-            throw new System.ArgumentException();
+            throw new System.ArgumentException("Invalid Values");
         }
         b = b / a;
         c = c / a;
@@ -28,9 +28,17 @@ public class SquareEquation
             answerOne[0] = x1;
             return answerOne;
         }
+        else if (b < eps)
+        {
+            double x1 = -((b + Math.Sqrt(d)) / 2);
+            double x2 = c / x1;
+            answerTwo[0] = x1;
+            answerTwo[1] = x2;
+            return answerTwo;
+        }
         else
         {
-            double x1 = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
+            double x1 = -((b + Math.Sign(b) * Math.Sqrt(d)) / 2);
             double x2 = c / x1;
             answerTwo[0] = x1;
             answerTwo[1] = x2;
